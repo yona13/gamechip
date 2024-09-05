61dc1bf012f5a4000f39a5dd9baf1e7a707993ba
Yona <yona.sandt@gmail.com>
2024-09-05 10:50:11 +0930

    Attempting to get Fonts and Icons on gh-pages

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const json5 = require("json5");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
@@ -20,5 +21,28 @@ module.exports = {
         filename: "[name].bundle.js",
         path: path.resolve(__dirname, "dist"),
         clean: true,
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.json5$/i,
+                type: "json",
+                parser: {
+                    parse: json5.parse,
+                }
+            },
+        ],
+    },
 }
